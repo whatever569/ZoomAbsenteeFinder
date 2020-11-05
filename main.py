2f5b79244a243b27c707f6f30a9fab2456ccc0d6
whatever569 <69293747+whatever569@users.noreply.github.com>
2020-11-06 02:40:32 +0300

    Added final code
    The code can recognize text from an image and compare that text to a list... if an element isn't found in the text, it will be apended to a list called absentees, which later will be printed in the console

---
+++ main.py
@@ -0,0 +1,71 @@
+from PIL import Image
+import pytesseract
+
+images = []
+recText = ""
+txtFile = ""
+
+#gets inputs
+def setup():
+    imageCount = 0
+    global images, txtFile
+    while True:
+        imageCount = input("Image count: ")
+        try:
+            imageCount = int(imageCount)
+            break
+        except ValueError:
+            print("Please input a valid value")
+            continue
+    for i in range(imageCount):
+        images.append(input(f"Image{i + 1} Path: "))
+    txtFile = input("Known names text: ")
+
+#recoginizes text from image
+def rec(imgUrl):
+
+    try:
+        img = Image.open(imgUrl)
+        return pytesseract.image_to_string(img)
+    except:
+        print("Image(s) path(s) is(are) invalid")
+        exit()
+
+
+#reads names from a .txt file, seperates them using the "," and stores the seperated names in an array
+def names(knownNamesTextFile):
+    tempStr = ""
+    f = open(knownNamesTextFile, 'r')
+    if not f == None:
+        names = f.read().split(",")
+        for i in range(len(names)):
+            for j in range(len(names[i])):
+                if names[i][j].isalpha():
+                    tempStr += names[i][j]
+            names[i] = tempStr.upper()
+            tempStr = ""
+        return names
+    else:
+        print(f"Known names file path <{knownNamesTextFile}> is invalid")
+        exit()
+
+#compares the list of names and the recognized text and finds the absentees
+def find(names, recognizedText):
+    absentees = []
+    for i in range(len(names)):
+        if not names[i] in recognizedText:
+            absentees.append(names[i])
+    return absentees
+
+#main execution
+if __name__ == "__main__":
+    setup()
+    tempText = ""
+    for img in images:
+        recText += rec(img)
+    for i in range(len(recText)):
+        if recText[i].isalpha():
+            tempText += recText[i]
+    recText = tempText.upper()
+
+    print(f"absentees: {find(names(txtFile), recText)}")
